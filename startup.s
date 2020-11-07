05c1ae439ebf9c727fda3aa1194b64999cbd1591
John de la Garza <jdelagarza@cylance.com>
2020-11-07 15:31:18 -0800

    refactor

--- startup.s
+++ startup.s
@@ -1,29 +1,59 @@
-        .global _start
-        .text
-        .code 32
+.data
+array_buff:
+	.word 3, 2, 1, 9, 0
+.text
+.code 32
 
-        @ Register  definition, for ARM VersatilePB with PL011 UART
-        .equ    UART0_BASE, 0x101f1000
-        .equ    UARTDR,     0x0
+@ Register  definition, for ARM VersatilePB with PL011 UART
+.equ    UART0_BASE, 0x101f1000
+.equ    UARTDR,     0x0
+
 .global _Reset
+
 _Reset:
-        @ Display 'str' to the UART:
-        ldr     r0, =str
-        ldr     r1, =UART0_BASE
+	b start
+	b .
+	b . /* 0x8  Software Interrupt */
+	b .
+	b .
+	b . /* 0x14 Reserved */
+	b . /* 0x18 IRQ */
+	b . /* 0x1C FIQ */
+
+start:
+	LDR sp, =stack_top
+	bl kernel_main
+	b .
+
+kernel_main:
+	bl init
+	bl hello
+
+	push {r11, lr}
+	add r11, sp, #0
+	sub sp, sp, #16
+	mov r0, #3
+	mov r1, #4
+	bl add
+	sub sp, r11, #0
+	pop {r11, pc}
+
+init:	ldr	r0, =str
+	ldr	r1, =UART0_BASE
 1:      ldrb    r2, [r0], #1     @ Get next character
         cmp     r2, #0            @ ... until end of string
-        beq     done                 @ We are done.
+        beq     done
         str     r2, [r1, #UARTDR] @ Print the character to UART
-        b       1b                @ Loop back
-done:
- LDR sp, =stack_top
- BL c_entry
- mov r2, #2
- mov r3, #3
- add r1, r2, r3
+        b       1b
+done:	bx lr
 
- b .
+add1:
+	push {r11}
+	add r11, sp, #0
+	sub sp, sp, #12
+a	add r0, r1
+	add sp, r11, #0
+	pop {r11}
+	bx lr
 
-str:    .asciz  "Hello world asm!\n"
-
-        .end
+str:    .asciz  "jjos initialized\n"
--- test.c
+++ test.c
@@ -1,12 +1,23 @@
 volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
 
-void print_uart0(const char *s) {
- while(*s != '\0') { /* Loop until end of string */
- *UART0DR = (unsigned int)(*s); /* Transmit char */
- s++; /* Next char */
- }
+void print_char(const char c)
+{
+  *UART0DR = (unsigned int)c; /* Transmit char */
 }
 
-void c_entry() {
- print_uart0("Hello world C!\n");
+void print_ascii(const char *s) {
+  while(*s != '\0')
+    print_char(*s++);
+}
+
+void hello() {
+  print_ascii("Hello world C!\n");
+}
+
+int add(int x, int y)
+{
+  int res = x + y;
+  print_char(((char)res)+48);
+  print_char('\n');
+  return res;
 }
